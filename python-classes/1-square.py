5810d543d4d669f480c573161e3fdc838159d51c
MrNeonn75 <devneonn75@gmail.com>
2024-02-05 11:33:02 +0400

    bug fix

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""
-s
-A Square Class
-a
-"""
+"""Write a class Square that defines a square by: (based on 0-square.py)
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module"""
 
 
 class Square:
