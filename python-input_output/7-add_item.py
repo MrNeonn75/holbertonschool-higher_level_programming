aa757b4e886d3bf4abe08ec27aa30a60f661ce33
MrNeonn75 <devneonn75@gmail.com>
2024-02-11 18:22:16 +0400

    Python - Input/Output bug fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,8 +8,8 @@ save them to a file
 
 from sys import argv
 import json
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 """ Module is documented """
 
