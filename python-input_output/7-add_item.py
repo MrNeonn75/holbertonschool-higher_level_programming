d37e14be8d08878533a6b8938b17be74122b8bb4
MrNeonn75 <devneonn75@gmail.com>
2024-02-11 18:20:34 +0400

    Python - Input/Output bug fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" Module is documented """
-
-from sys import argv
-import json
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 """
 Write a script that adds all
 arguments to a Python list, and then
 save them to a file
 """
 
+
+from sys import argv
+import json
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+""" Module is documented """
 
 argc = len(argv)
 
