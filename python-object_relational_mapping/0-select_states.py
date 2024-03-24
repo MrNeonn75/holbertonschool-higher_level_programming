f2929900a790f0e26bc2ccddd88a05a468f14c19
MrNeonn75 <devneonn75@gmail.com>
2024-03-24 19:27:36 +0400

    Python - Object-relational mapping

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,46 +1,13 @@
 #!/usr/bin/python3
-
-"""
-Lists all states from the database hbtn_0e_0_usa.
-"""
-
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
 import MySQLdb
 
-def main():
-    # Variables for getting arguments
-    username = sys.argv[0]
-    password = sys.argv[1]
-    database_name = sys.argv[2]
-    
-    # Connect to MySQL server
-    server = MySQLdb.connect(
-        host = "localhost",
-        port = 3306,
-        user = username,
-        passwd = password,
-        db = database_name
-    )
-    
-    # A variable which we can use for working with server
-    cursor = server.cursor()
-    
-    # A variable which we save request query
-    query = "SELECT * FROM states"
-    
-    # Execute MySQL command
-    cursor.execute(query)
-    
-    # Get all rows
-    results = cursor.fetchall()
-    
-    # Display the result 
-    for data in results:
-        print(data)
-        
-    # Close connection
-    cursor.close()
-    server.close()
-
-if (__name__ == "__main__"):
-    main()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,47 +1,13 @@
 #!/usr/bin/python3
-
-"""
-Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
-"""
-
+# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+# Usage: ./1-filter_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
 import MySQLdb
 
-def main():
-    # The variables for getting data.
-    username = sys.argv[0]
-    password = sys.argv[1]
-    database = sys.argv[2]
-    
-    # Connect to MySQL server
-    server = MySQLdb.connect(
-        host = "localhost",
-        port = 3306,
-        user = username,
-        passwd = password,
-        db = database
-    )
-    
-    # Cursor
-    cursor = server.cursor()
-    
-    # Query
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"
-    
-    # Send request
-    cursor.execute(query)
-    
-    # Get result
-    result = cursor.fetchall()
-    
-    # Print result
-    for data in result:
-        print(data)
-    
-    # Close connection
-    cursor.close()
-    server.close()
-    
-if (__name__ == "__main__"):
-    main()
-    
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
