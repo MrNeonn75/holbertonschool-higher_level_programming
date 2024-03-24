e75d211714be99b57e17d17fbe3c451c80204747
MrNeonn75 <devneonn75@gmail.com>
2024-03-24 22:04:51 +0400

    Python - Object-relational mapping Bug fix

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,42 @@
 #!/usr/bin/python3
-
 """
-Once again, write a script that takes in arguments
-and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
-But this time, write one that
-is safe from MySQL injections!
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+
+This time the script is safe from
+MySQL injections!
 """
 
-from sys import argv
 import MySQLdb
-    
-if __name__ == "__main__":
-    server = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cursor = server.cursor()
-    cursor.execute("""SELECT * FROM `states`""")
+from sys import argv
 
-    [print(state) for state in cursor.fetchall() if state[1] == argv[4]]
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
-    cursor.close()
-    server.close()
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
+
+        rows = cur.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
